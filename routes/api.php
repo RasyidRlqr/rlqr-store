c50c5ef6d600eed5a2bd3ef46a5793d022925ec3
RasyidRlqr <rasyidrlqr@gmail.com>
2025-11-10 16:14:34 +0700

    up integration bot discord

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,5 @@
 <?php
-
 // routes/api.php
-
 use App\Http\Controllers\OrderController; 
 
-// Route khusus Bot. Menggunakan POST karena ini adalah perintah perubahan data.
 Route::post('/bot/update-order-status', [OrderController::class, 'updateStatusFromBot']);
